a6422547ebec8dc8fd224e1a632215f0e7ed1d0a
angelom88 <angelo.matildo@mnltechnology.com>
2017-01-10 18:50:30 +0800

    added and removed 2 images

--- docs/changes.js
+++ docs/changes.js
@@ -1,7 +1,7 @@
-var a = "bnr-BO-Live-Casino-Turbo.jpg"
-var b = "bnr-BO-3D-Slots-Four-Guardians.jpg"
-var c = "squeeze.jpg"
-var d = "pimgpsh_fullsize_distr.jpg"
+var a = "squeeze.jpg"
+var b = "pimgpsh_fullsize_distr.jpg"
+var c = "bnr-BO-Live-Casino-Super-3-Pictures.jpg"
+var d = "bnr-BO-3D-Slots-Fu-Lu-Shou.jpg"
 banners([
 a,b,c,d
 ])

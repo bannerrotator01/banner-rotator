c1680b6128b103c2bcb83d0a1113bac2f9bf2557
angelom88 <angelo.matildo@mnltechnology.com>
2017-06-27 10:44:06 +0800

    added and replace images 5.18.2017

--- docs/changes.js
+++ docs/changes.js
@@ -1,7 +1,7 @@
 var a = "squeeze.jpg"
-var b = "pimgpsh_fullsize_distr.jpg"
-var c = "bnr-BO-Live-Casino-Super-3-Pictures.jpg"
-var d = "bnr-BO-3D-Slots-Fu-Lu-Shou.jpg"
+var b = "bnr-BO-Live-Casino-Super-3-Pictures.jpg"
+var c = "bnr-BO-3D-Slots-Sweet-Treats.jpg"
+var d = "bnr-BO-3D-Slots-Fortune-Dice.jpg"
 banners([
 a,b,c,d
 ])

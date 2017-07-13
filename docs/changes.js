83dfe01ee7572aa8f82c5a5fc4b1530bd4cbc64c
angelom88 <angelo.matildo@mnltechnology.com>
2017-07-13 09:45:03 +0800

    added banner for super hilo

--- docs/changes.js
+++ docs/changes.js
@@ -2,6 +2,7 @@ var a = "squeeze.jpg"
 var b = "bnr-BO-Live-Casino-Super-3-Pictures.jpg"
 var c = "bnr-BO-3D-Slots-Sweet-Treats.jpg"
 var d = "bnr-BO-3D-Slots-Fortune-Dice.jpg"
+var e = "bnr-BO-Live-Casino-Super-Hi-Lo"
 banners([
-a,b,c,d
+a,b,c,d,e
 ])

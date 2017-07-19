a26e509e3cdf843dd3dfc610d767dabbb6d1b3c6
ngelmat43 <ngelmat43@gmail.com>
2017-07-19 14:34:53 +0800

    added banner for slots

--- docs/changes.js
+++ docs/changes.js
@@ -3,6 +3,7 @@ var b = "bnr-BO-Live-Casino-Super-3-Pictures.jpg"
 var c = "bnr-BO-3D-Slots-Sweet-Treats.jpg"
 var d = "bnr-BO-3D-Slots-Fortune-Dice.jpg"
 var e = "bnr-BO-Live-Casino-Super-Hi-Lo.jpg"
+var f = "bnr-BO-3D-Slots-Cleopatra.jpg"
 banners([
-e,d,c,b,a
+f,e,d,c,b,a
 ])

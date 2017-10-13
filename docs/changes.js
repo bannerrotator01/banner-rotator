5f166e942041f76012e61e6d0549217d673fb4ea
angelom88 <ngelmat43@gmail.com>
2017-10-13 12:19:26 +0800

    replace fortune dice with lucky royale

--- docs/changes.js
+++ docs/changes.js
@@ -1,9 +1,9 @@
-var a = "squeeze.jpg"
-var b = "bnr-BO-Live-Casino-Super-3-Pictures.jpg"
-var c = "bnr-BO-3D-Slots-Sweet-Treats.jpg"
-var d = "bnr-BO-3D-Slots-Fortune-Dice.jpg"
-var e = "bnr-BO-Live-Casino-Super-Hi-Lo.jpg"
-var f = "bnr-BO-3D-Slots-Cleopatra.jpg"
+var a = 'lucky-royale-bo.jpg'
+var b = 'squeeze.jpg'
+var c = 'bnr-BO-Live-Casino-Super-3-Pictures.jpg'
+var d = 'bnr-BO-3D-Slots-Sweet-Treats.jpg'
+var e = 'bnr-BO-Live-Casino-Super-Hi-Lo.jpg'
+var f = 'bnr-BO-3D-Slots-Cleopatra.jpg'
 banners([
 f,e,d,c,b,a
 ])

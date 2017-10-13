30a0e81b55af0555589b5d9019051e9312d21bcf
angelom88 <ngelmat43@gmail.com>
2017-10-13 12:22:39 +0800

    fixed order

--- docs/changes.js
+++ docs/changes.js
@@ -1,9 +1,10 @@
-var a = 'lucky-royale-bo.jpg'
-var b = 'squeeze.jpg'
-var c = 'bnr-BO-Live-Casino-Super-3-Pictures.jpg'
-var d = 'bnr-BO-3D-Slots-Sweet-Treats.jpg'
-var e = 'bnr-BO-Live-Casino-Super-Hi-Lo.jpg'
-var f = 'bnr-BO-3D-Slots-Cleopatra.jpg'
+var a = 'squeeze.jpg'
+var b = 'bnr-BO-Live-Casino-Super-3-Pictures.jpg'
+var c = 'bnr-BO-3D-Slots-Sweet-Treats.jpg'
+var d = 'bnr-BO-Live-Casino-Super-Hi-Lo.jpg'
+var e = 'bnr-BO-3D-Slots-Cleopatra.jpg'
+var f = 'lucky-royale-bo.jpg'
+
 banners([
 f,e,d,c,b,a
 ])

fa74bb8259503152347b0d09b8204c3112e085b1
angelom88 <ngelmat43@gmail.com>
2017-11-07 13:31:44 +0800

    Monsters Cash to replace Cleopatra

--- docs/changes.js
+++ docs/changes.js
@@ -2,8 +2,8 @@ var a = 'squeeze.jpg'
 var b = 'bnr-BO-Live-Casino-Virtual-Dealer-Baccarat.jpg'
 var c = 'bnr-BO-3D-Slots-Lucky-Bomber.jpg'
 var d = 'bnr-BO-Live-Casino-Super-Hi-Lo.jpg'
-var e = 'bnr-BO-3D-Slots-Cleopatra.jpg'
-var f = 'lucky-royale-bo.jpg'
+var e = 'lucky-royale-bo.jpg'
+var f = 'bnr-BO-3D-Slots-Monsters-Cash.jpg'
 
 banners([
 f,e,d,c,b,a

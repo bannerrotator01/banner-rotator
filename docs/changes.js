8c8d01384f103f94de0d95fdcadb826e3f882a98
angelom88 <ngelmat43@gmail.com>
2017-11-23 15:34:26 +0800

    add lion dance and remove lucky bomber

--- docs/changes.js
+++ docs/changes.js
@@ -1,9 +1,9 @@
 var a = 'squeeze.jpg'
 var b = 'bnr-BO-Live-Casino-Virtual-Dealer-Baccarat.jpg'
-var c = 'bnr-BO-3D-Slots-Lucky-Bomber.jpg'
-var d = 'bnr-BO-Live-Casino-Super-Hi-Lo.jpg'
-var e = 'lucky-royale-bo.jpg'
-var f = 'bnr-BO-3D-Slots-Monsters-Cash.jpg'
+var c = 'bnr-BO-Live-Casino-Super-Hi-Lo.jpg'
+var d = 'lucky-royale-bo.jpg'
+var e = 'bnr-BO-3D-Slots-Monsters-Cash.jpg'
+var f = 'BO-Lion-Dance.jpg'
 
 banners([
 f,e,d,c,b,a

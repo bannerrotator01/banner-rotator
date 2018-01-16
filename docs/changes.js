ed8134ec6c073d7729b45cf4d367063bb8916b7b
angelom88 <ngelmat43@gmail.com>
2018-01-16 17:17:05 +0800

    new banner replace lucy royale

--- docs/changes.js
+++ docs/changes.js
@@ -1,10 +1,10 @@
 var a = 'squeeze.jpg'
 var b = 'bnr-BO-Live-Casino-Virtual-Dealer-Baccarat.jpg'
 var c = 'bnr-BO-Live-Casino-Super-Hi-Lo.jpg'
-var d = 'lucky-royale-bo.jpg'
-var e = 'bnr-BO-3D-Slots-Monsters-Cash.jpg'
-var f = 'BO-Lion-Dance.jpg'
-var g = 'bnr-BO-Live-Casino-Lucky-Baccarat.jpg'
+var d = 'bnr-BO-3D-Slots-Monsters-Cash.jpg'
+var e = 'BO-Lion-Dance.jpg'
+var f = 'bnr-BO-Live-Casino-Lucky-Baccarat.jpg'
+var g = 'bnr-BO-3D-Slots-Wilds-and-the-Beanstalk.jpg'
 
 banners([
 g,f,e,d,c,b,a

5476949c4ccba63c5917475ecdd40fb464f5616c
angelom88 <ngelmat43@gmail.com>
2018-04-11 18:28:42 +0800

    replace banner

--- docs/changes.js
+++ docs/changes.js
@@ -3,9 +3,9 @@ var b = 'bnr-BO-Live-Casino-Virtual-Dealer-Baccarat.jpg'
 var c = 'bnr-BO-Live-Casino-Super-Hi-Lo.jpg'
 var d = 'BO-Lion-Dance.jpg'
 var e = 'bnr-BO-Live-Casino-Lucky-Baccarat.jpg'
-var f = 'bnr-BO-3D-Slots-Wilds-and-the-Beanstalk.jpg'
-var g = 'bnr-BO-3D-Slots-Blossom-Garden.jpg'
-var h = 'bnr-BO-3D-Slots-Wuxia-Princess.jpg'
+var f = 'bnr-BO-3D-Slots-Blossom-Garden.jpg'
+var g = 'bnr-BO-3D-Slots-Wuxia-Princess.jpg'
+var h = 'bnr-BO-3D-Slots-Phantom-Thief.jpg'
 
 banners([
 h,g,f,e,d,c,b,a

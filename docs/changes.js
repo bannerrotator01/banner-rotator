dc86a382687402da4cb88b2e37465a946ec6f01d
angelom88 <ngelmat43@gmail.com>
2018-05-17 14:57:25 +0800

    add new banner and replace wuxia image

--- docs/changes.js
+++ docs/changes.js
@@ -3,9 +3,9 @@ var b = 'bnr-BO-Live-Casino-Virtual-Dealer-Baccarat.jpg'
 var c = 'bnr-BO-Live-Casino-Super-Hi-Lo.jpg'
 var d = 'BO-Lion-Dance.jpg'
 var e = 'bnr-BO-Live-Casino-Lucky-Baccarat.jpg'
-var f = 'bnr-BO-3D-Slots-Wuxia-Princess.jpg'
-var g = 'bnr-BO-3D-Slots-Phantom-Thief.jpg'
-var h = 'bnr-BO-3D-Slots-Genies-Luck.jpg'
+var f = 'bnr-BO-3D-Slots-Phantom-Thief.jpg'
+var g = 'bnr-BO-3D-Slots-Genies-Luck.jpg'
+var h = 'bnr-BO-3D-Slots-World-Soccer-Slot-2.jpg'
 
 banners([
 h,g,f,e,d,c,b,a

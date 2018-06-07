a8c3c06485d36428fa9b02c7734d144536d3995e
angelom88 <ngelmat43@gmail.com>
2018-06-07 12:14:18 +0800

    replace lion dance with 3D slots kungfu

--- docs/changes.js
+++ docs/changes.js
@@ -1,11 +1,12 @@
 var a = 'squeeze.jpg'
 var b = 'bnr-BO-Live-Casino-Virtual-Dealer-Baccarat.jpg'
 var c = 'bnr-BO-Live-Casino-Super-Hi-Lo.jpg'
-var d = 'BO-Lion-Dance.jpg'
-var e = 'bnr-BO-Live-Casino-Lucky-Baccarat.jpg'
-var f = 'bnr-BO-3D-Slots-Phantom-Thief.jpg'
-var g = 'bnr-BO-3D-Slots-Genies-Luck.jpg'
-var h = 'bnr-BO-3D-Slots-World-Soccer-Slot-2.jpg'
+var d = 'bnr-BO-Live-Casino-Lucky-Baccarat.jpg'
+var e = 'bnr-BO-3D-Slots-Phantom-Thief.jpg'
+var f = 'bnr-BO-3D-Slots-Genies-Luck.jpg'
+var g = 'bnr-BO-3D-Slots-World-Soccer-Slot-2.jpg'
+var h = 'bnr-BO-3D-Slots-Kung-Fu-Furry.jpg'
+
 
 banners([
 h,g,f,e,d,c,b,a

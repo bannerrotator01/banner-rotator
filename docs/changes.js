195375415aac4fcfa8d59be562db4085b731097c
angelom88 <ngelmat43@gmail.com>
2018-07-13 16:29:44 +0800

    replace phantom thief

--- docs/changes.js
+++ docs/changes.js
@@ -2,10 +2,10 @@ var a = 'squeeze.jpg'
 var b = 'bnr-BO-Live-Casino-Virtual-Dealer-Baccarat.jpg'
 var c = 'bnr-BO-Live-Casino-Super-Hi-Lo.jpg'
 var d = 'bnr-BO-Live-Casino-Lucky-Baccarat.jpg'
-var e = 'bnr-BO-3D-Slots-Phantom-Thief.jpg'
-var f = 'bnr-BO-3D-Slots-Genies-Luck.jpg'
-var g = 'bnr-BO-3D-Slots-World-Soccer-Slot-2.jpg'
-var h = 'bnr-BO-3D-Slots-Kung-Fu-Furry.jpg'
+var e = 'bnr-BO-3D-Slots-Genies-Luck.jpg'
+var f = 'bnr-BO-3D-Slots-World-Soccer-Slot-2.jpg'
+var g = 'bnr-BO-3D-Slots-Kung-Fu-Furry.jpg'
+var h = 'bnr-BO-3D-Slots-Gem-Forest.jpg'
 
 
 banners([

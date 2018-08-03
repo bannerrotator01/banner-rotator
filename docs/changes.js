93ed222e46364781a1b8ceaa8f8f04d983673509
angelom88 <ngelmat43@gmail.com>
2018-08-03 15:01:36 +0800

    replace genie's luck with frost dragon

--- docs/changes.js
+++ docs/changes.js
@@ -2,11 +2,11 @@ var a = 'squeeze.jpg'
 var b = 'bnr-BO-Live-Casino-Virtual-Dealer-Baccarat.jpg'
 var c = 'bnr-BO-Live-Casino-Super-Hi-Lo.jpg'
 var d = 'bnr-BO-Live-Casino-Lucky-Baccarat.jpg'
-var e = 'bnr-BO-3D-Slots-Genies-Luck.jpg'
-var f = 'bnr-BO-3D-Slots-World-Soccer-Slot-2.jpg'
-var g = 'bnr-BO-3D-Slots-Kung-Fu-Furry.jpg'
-var h = 'bnr-BO-3D-Slots-Gem-Forest.jpg'
-var i = 'bnr-BO-Games-Rock-Paper-Scissors.jpg'
+var e = 'bnr-BO-3D-Slots-World-Soccer-Slot-2.jpg'
+var f = 'bnr-BO-3D-Slots-Kung-Fu-Furry.jpg'
+var g = 'bnr-BO-3D-Slots-Gem-Forest.jpg'
+var h = 'bnr-BO-Games-Rock-Paper-Scissors.jpg'
+var i = 'bnr-BO-3D-Slots-Frost-Dragon.jpg'
 
 
 banners([

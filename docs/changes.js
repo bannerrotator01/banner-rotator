febfbc9d4c8a5a2b9aa93a4ee4bd8478184a6959
angelom88 <ngelmat43@gmail.com>
2018-08-20 10:52:00 +0800

    replace gemforest with jewel land

--- docs/changes.js
+++ docs/changes.js
@@ -4,11 +4,12 @@ var c = 'bnr-BO-Live-Casino-Super-Hi-Lo.jpg'
 var d = 'bnr-BO-Live-Casino-Lucky-Baccarat.jpg'
 var e = 'bnr-BO-3D-Slots-World-Soccer-Slot-2.jpg'
 var f = 'bnr-BO-3D-Slots-Kung-Fu-Furry.jpg'
-var g = 'bnr-BO-3D-Slots-Gem-Forest.jpg'
-var h = 'bnr-BO-Games-Rock-Paper-Scissors.jpg'
-var i = 'bnr-BO-3D-Slots-Frost-Dragon.jpg'
+var g = 'bnr-BO-Games-Rock-Paper-Scissors.jpg'
+var h = 'bnr-BO-3D-Slots-Frost-Dragon.jpg'
+var i = 'bnr-BO-3D-Slots-Jewel-Land.jpg'
+
 
 
 banners([
-i,h,g,f,e,d,c,b,a
+    i, h, g, f, e, d, c, b, a
 ])

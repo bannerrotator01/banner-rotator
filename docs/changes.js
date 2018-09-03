9e3032787efbdf745a4b012e890d239922b2a63b
angelom88 <ngelmat43@gmail.com>
2018-09-03 17:51:11 +0800

    replace world cup with monkey monkey

--- docs/changes.js
+++ docs/changes.js
@@ -2,11 +2,11 @@ var a = 'squeeze.jpg'
 var b = 'bnr-BO-Live-Casino-Virtual-Dealer-Baccarat.jpg'
 var c = 'bnr-BO-Live-Casino-Super-Hi-Lo.jpg'
 var d = 'bnr-BO-Live-Casino-Lucky-Baccarat.jpg'
-var e = 'bnr-BO-3D-Slots-World-Soccer-Slot-2.jpg'
-var f = 'bnr-BO-3D-Slots-Kung-Fu-Furry.jpg'
-var g = 'bnr-BO-Games-Rock-Paper-Scissors.jpg'
-var h = 'bnr-BO-3D-Slots-Frost-Dragon.jpg'
-var i = 'bnr-BO-3D-Slots-Jewel-Land.jpg'
+var e = 'bnr-BO-3D-Slots-Kung-Fu-Furry.jpg'
+var f = 'bnr-BO-Games-Rock-Paper-Scissors.jpg'
+var g = 'bnr-BO-3D-Slots-Frost-Dragon.jpg'
+var h = 'bnr-BO-3D-Slots-Jewel-Land.jpg'
+var i = 'bnr-BO-3D-Slots-Money-Monkey.jpg'
 
 
 

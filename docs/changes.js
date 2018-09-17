06bfa1863a2880b8b45d60e3606a3ae03c5807fe
angelom88 <ngelmat43@gmail.com>
2018-09-17 10:20:34 +0800

    replace kungfu furry

--- docs/changes.js
+++ docs/changes.js
@@ -2,12 +2,12 @@ var a = 'squeeze.jpg'
 var b = 'bnr-BO-Live-Casino-Virtual-Dealer-Baccarat.jpg'
 var c = 'bnr-BO-Live-Casino-Super-Hi-Lo.jpg'
 var d = 'bnr-BO-Live-Casino-Lucky-Baccarat.jpg'
-var e = 'bnr-BO-3D-Slots-Kung-Fu-Furry.jpg'
-var f = 'bnr-BO-Games-Rock-Paper-Scissors.jpg'
-var g = 'bnr-BO-3D-Slots-Frost-Dragon.jpg'
-var h = 'bnr-BO-3D-Slots-Jewel-Land.jpg'
-var i = 'bnr-BO-3D-Slots-Money-Monkey.jpg'
-var j = 'bnr-BO-Live-Casino-Grand.jpg'
+var e = 'bnr-BO-Games-Rock-Paper-Scissors.jpg'
+var f = 'bnr-BO-3D-Slots-Frost-Dragon.jpg'
+var g = 'bnr-BO-3D-Slots-Jewel-Land.jpg'
+var h = 'bnr-BO-3D-Slots-Money-Monkey.jpg'
+var i = 'bnr-BO-Live-Casino-Grand.jpg'
+var j = 'bnr-BO-3D-Slots-Moon-Rabbit.jpg'
 
 
 

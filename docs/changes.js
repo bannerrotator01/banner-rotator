661fe823b2191bfa906be40c767b5b2a8605bd3f
angelom88 <ngelmat43@gmail.com>
2018-10-16 10:04:11 +0800

    replace with the new banner

--- docs/changes.js
+++ docs/changes.js
@@ -3,11 +3,11 @@ var b = 'bnr-BO-Live-Casino-Virtual-Dealer-Baccarat.jpg'
 var c = 'bnr-BO-Live-Casino-Super-Hi-Lo.jpg'
 var d = 'bnr-BO-Live-Casino-Lucky-Baccarat.jpg'
 var e = 'bnr-BO-Games-Rock-Paper-Scissors.jpg'
-var f = 'bnr-BO-3D-Slots-Jewel-Land.jpg'
-var g = 'bnr-BO-3D-Slots-Money-Monkey.jpg'
-var h = 'bnr-BO-Live-Casino-Grand.jpg'
-var i = 'bnr-BO-3D-Slots-Moon-Rabbit.jpg'
-var j = 'bnr-BO-3D-Slots-Alchemists-Spell.jpg'
+var f = 'bnr-BO-3D-Slots-Money-Monkey.jpg'
+var g = 'bnr-BO-Live-Casino-Grand.jpg'
+var h = 'bnr-BO-3D-Slots-Moon-Rabbit.jpg'
+var i = 'bnr-BO-3D-Slots-Alchemists-Spell.jpg'
+var j = 'bnr-BO-3D-Slots-Chess-Royale.jpg'
 
 
 

df35bb600b8960cfd33584b9d8b419e0abb6c04b
angelom88 <ngelmat43@gmail.com>
2018-12-17 18:37:08 +0800

    replace alchemist with fortune and bullfight

--- docs/changes.js
+++ docs/changes.js
@@ -4,13 +4,13 @@ var c = 'bnr-BO-Live-Casino-Super-Hi-Lo.jpg'
 var d = 'bnr-BO-Live-Casino-Lucky-Baccarat.jpg'
 var e = 'bnr-BO-Games-Rock-Paper-Scissors.jpg'
 var f = 'bnr-BO-Live-Casino-Grand.jpg'
-var g = 'bnr-BO-3D-Slots-Alchemists-Spell.jpg'
-var h = 'bnr-BO-3D-Slots-Chess-Royale.jpg'
-var i = 'bnr-BO-3D-Slots-Space-Neon.jpg'
-var j = 'bnr-BO-3D-Slots-Jazz-It-Up.jpg'
-
+var g = 'bnr-BO-3D-Slots-Chess-Royale.jpg'
+var h = 'bnr-BO-3D-Slots-Space-Neon.jpg'
+var i = 'bnr-BO-3D-Slots-Jazz-It-Up.jpg'
+var j = 'bnr-BO-3D-Slots-Fountain-of-Fortune.jpg'
+var k = 'Banner-BO-BullFight.jpg'
 
 
 banners([
-    j, i, h, g, f, e, d, c, b, a
+    k, j, i, h, g, f, e, d, c, b, a
 ])

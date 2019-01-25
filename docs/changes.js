b2811c506a658c31aea455f0a3d14a6b5cf3a0e4
angelom88 <ngelmat43@gmail.com>
2019-01-25 11:09:33 +0800

    replace chess royale with the new banner

--- docs/changes.js
+++ docs/changes.js
@@ -4,11 +4,11 @@ var c = 'bnr-BO-Live-Casino-Super-Hi-Lo.jpg'
 var d = 'bnr-BO-Live-Casino-Lucky-Baccarat.jpg'
 var e = 'bnr-BO-Games-Rock-Paper-Scissors.jpg'
 var f = 'bnr-BO-Live-Casino-Grand.jpg'
-var g = 'bnr-BO-3D-Slots-Chess-Royale.jpg'
-var h = 'bnr-BO-3D-Slots-Space-Neon.jpg'
-var i = 'bnr-BO-3D-Slots-Jazz-It-Up.jpg'
-var j = 'bnr-BO-3D-Slots-Fountain-of-Fortune.jpg'
-var k = 'Banner-BO-BullFight.jpg'
+var g = 'bnr-BO-3D-Slots-Space-Neon.jpg'
+var h = 'bnr-BO-3D-Slots-Jazz-It-Up.jpg'
+var i = 'bnr-BO-3D-Slots-Fountain-of-Fortune.jpg'
+var j = 'Banner-BO-BullFight.jpg'
+var k = 'bnr-BO-3D-Slots-Fa-Fa-Zhu.jpg'
 
 
 banners([

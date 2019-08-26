1c703b7580391ac76859f8f8448c9090178042b7
jacy <johnsoncr.hong@gmail.com>
2019-08-26 17:27:22 +0800

    replace 2 new games

--- docs/changes.js
+++ docs/changes.js
@@ -4,12 +4,12 @@ var c = 'bnr-BO-Live-Casino-Super-Hi-Lo.jpg'
 var d = 'bnr-BO-Live-Casino-Lucky-Baccarat.jpg'
 var e = 'bnr-BO-Games-Thor.jpg'
 var f = 'bnr-BO-Live-Casino-Grand.jpg'
-var g = 'bnr-BO-3D-Slots-Magic-Paper.jpg'
+var g = 'bnr-BO-3D-Slots-Fortune-Hong-Bao.jpg'
 var h = 'bnr-BO-3D-Slots-Panda-Warrior.jpg'
 var i = 'bnr-BO-3D-Slots-Captain-Rabbit.jpg'
 var j = 'bnr-BO-3D-Slots-StripnRoll.jpg'
 var k = 'bnr-BO-3D-Slots-Fa-Fa-Zhu.jpg'
-var l = 'bnr-BO-3D-Slots-Lucky-Giant.jpg'
+var l = 'bnr-BO-3D-Slots-Nuwa-and-the-Five-Elements.jpg'
 
 
 banners([

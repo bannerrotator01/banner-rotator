441cb0b4a8c6cd70740f036c589fb1017e78d8cd
jacy <johnsoncr.hong@gmail.com>
2019-11-20 11:38:02 +0800

    display new game and remove old game

--- docs/changes.js
+++ docs/changes.js
@@ -7,7 +7,7 @@ var f = 'bnr-BO-Live-Casino-Grand.jpg'
 var g = 'bnr-BO-3D-Slots-Fortune-Hong-Bao.jpg'
 var h = 'bnr-BO-3D-Slots-Panda-Warrior.jpg'
 var i = 'bnr-BO-3D-Slots-Captain-Rabbit.jpg'
-var j = 'bnr-BO-3D-Slots-StripnRoll.jpg'
+var j = 'bnr-BO-3D-Slots-Lunar-Legends.jpg'
 var k = 'bnr-BO-3D-Slots-Cosmic-Boost.jpg'
 var l = 'bnr-BO-3D-Slots-Nuwa-and-the-Five-Elements.jpg'
 

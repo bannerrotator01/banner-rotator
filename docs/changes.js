ffbf469a42b9a49670dbe97034f7bf77d191cdb4
jacy <johnsoncr.hong@gmail.com>
2020-03-12 15:32:31 +0800

    display new game

--- docs/changes.js
+++ docs/changes.js
@@ -8,10 +8,11 @@ var g = 'bnr-BO-3D-Slots-Fortune-Hong-Bao.jpg'
 var h = 'bnr-BO-3D-Slots-Panda-Warrior.jpg'
 var i = 'bnr-BO-3D-Slots-Captain-Rabbit.jpg'
 var j = 'bnr-BO-3D-Slots-Lunar-Legends.jpg'
-var k = 'bnr-BO-3D-Slots-Cosmic-Boost.jpg'
-var l = 'bnr-BO-3D-Slots-Nation-Double-Play.jpg'
+var k = 'bnr-BO-3D-Slots-Nation-Double-Play.jpg'
+var l = 'bnr-BO-Live-Casino-Super-Roulette.jpg'
+var m = 'bnr-BO-3D-Slots-Dino-Age.jpg'
 
 
 banners([
-    l,k, j, i, h, g, f, e, d, c, b, a
+    m,l,k, j, i, h, g, f, e, d, c, b, a
 ])

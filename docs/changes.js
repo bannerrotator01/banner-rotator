a4dd1cd5fbcf4acc60653f4dc1737a1bd94e7d8c
ngelmat <ngelmat43@gmail.com>
2021-10-19 17:50:32 +0800

    replace banner

--- docs/changes.js
+++ docs/changes.js
@@ -4,7 +4,6 @@ var c = 'bnr-BO-Live-Casino-Super-Hi-Lo.jpg'
 var d = 'bnr-BO-Live-Casino-Lucky-Baccarat.jpg'
 var e = 'bnr-BO-Lottery-Xoc-Dia.jpg'
 var f = 'bnr-BO-Live-Casino-Grand.jpg'
-var g = 'bnr-BO-3D-Slots-UFO-Frenzy.jpg'
 var h = 'bnr-BO-Live-Casino-Super-Roulette.jpg'
 var i = 'bnr-BO-3D-Slots-Fortune-Dragon.jpg'
 var j = 'bnr-BO-3D-Slots-Songkran-Summer-Splash.jpg'
@@ -13,8 +12,8 @@ var l = 'bnr-BO-3D-Slots-Mine-of-Riches.jpg'
 var m = 'bnr-BO-Games-Thai-Hi-Lo-Deluxe.jpg'
 var n = 'bnr-BO-3D-Slots-Elf-Surprise-Jackpot.png'
 var o = 'bnr-BO-3D-Slots-Alpha-and-the-Red-Hood.jpg'
-
+var g = 'bnr-BO-3D-Slots-Sweet-Baker.jpg'
 
 banners([
-    o,l,m,g,n,k, j, i, h, f, e, d, c, b, a
+    g,o,l,m,n,k, j, i, h, f, e, d, c, b, a
 ])

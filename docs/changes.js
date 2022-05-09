74fe1aa9cd4f42a690aef8cd0d2e734e761eebba
jacy <johnsoncr.hong@gmail.com>
2022-05-09 10:46:55 +0800

    replace 2 new banners

--- docs/changes.js
+++ docs/changes.js
@@ -6,14 +6,14 @@ var e = 'bnr-BO-Lottery-Xoc-Dia.jpg'
 var f = 'bnr-BO-Live-Casino-Grand.jpg'
 var h = 'bnr-BO-Live-Casino-Super-Roulette.jpg'
 var i = 'bnr-BO-3D-Slots-Fortune-Dragon.jpg'
-var j = 'bnr-BO-3D-Slots-Songkran-Summer-Splash.jpg'
+var j = 'bnr-BO-Games-Three-Faces-Baccarat.jpg'
 var k = 'bnr-BO-3D-Slots-God-of-Flames.jpg'
 var l = 'bnr-BO-3D-Slots-Mine-of-Riches.jpg'
-var m = 'bnr-BO-Games-Thai-Hi-Lo-Deluxe.jpg'
+var m = 'bnr-BO-3D-Slots-Tigers-Roar.jpg'
 var o = 'bnr-BO-3D-Slots-Alpha-and-the-Red-Hood.jpg'
 var g = 'bnr-BO-3D-Slots-Sweet-Baker.jpg'
 var n = 'bnr-BO-3D-Slots-Skater-King.jpg'
 
 banners([
-    n,g,o,l,m,k, j, i, h, f, e, d, c, b, a
+    j, m, n, g, o, l, k, i, h, f, e, d, c, b, a
 ])

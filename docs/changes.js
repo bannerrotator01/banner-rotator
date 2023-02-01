f62915f6b9842d16999891536b6785ae5f273913
angelo <ngelmat54@gmail.com>
2023-02-01 15:47:48 +0800

    replace banners

--- docs/changes.js
+++ docs/changes.js
@@ -7,10 +7,10 @@ var f = 'bnr-BO-Live-Casino-Grand.jpg'
 var g = 'bnr-BO-Full-Moon-Legacy-Mega-Reels.jpg'
 var h = 'bnr-BO-Live-Casino-Super-Roulette.jpg'
 var i = 'bnr-BO-3D-Slots-Fortune-Dragon.jpg'
-var j = 'bnr-BO-Games-Three-Faces-Baccarat.jpg'
+var j = 'bnr-BO-Games-Coin-Toss-Deluxe.jpg'
 var k = 'bnr-BO-3D-Slots-God-of-Flames.jpg'
 var l = 'bnr-BO-3D-Slots-Rock-and-Rubbles.jpg'
-var m = 'bnr-BO-3D-Slots-Tigers-Roar.jpg'
+var m = 'bnr-BO-3D-Slots-Rainforest-Riches.jpg'
 var n = 'bnr-BO-3D-Slots-Skater-King.jpg'
 var o = 'bnr-BO-3D-Slots-Lucky-Meo-Meo.jpg'
 var p = 'bnr-BO-Fishing-Ocean-Explorer.jpg'
@@ -19,5 +19,5 @@ var r = 'bnr-BO-Games-Football-Lucky-Penalty-Kick.jpg'
 var s = 'bnr-BO-Games-Teen-Patti.jpg'
 
 banners([
-    s, o, l, g, r, q, p, j, m, n, k, i, h, f, e, d, c, b, a
+    m, j, s, o, l, g, r, q, p, n, k, i, h, f, e, d, c, b, a
 ])
